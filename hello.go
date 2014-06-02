048baadea4d78ad726e1e385fca843902893ff11
yantao <306932545@qq.com>
2014-06-02 22:01:47 +0800

    first commit

---
+++ hello.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+const PI = 3.14
+
+var name = "yt"
+
+type newType int
+
+type mystruct struct {
+}
+
+type golang interface{}
+
+func main() {
+	//fmt.Println("Hello world 书剑")
+	//a := [2]int{}
+	//fmt.Println(a)
+
+	// a := [2]int{1, 2}
+	// b := [2]int{2, 4}
+	// fmt.Println(a == b)
+	// fmt.Println(len(a))
+
+	// fmt.Fprintln(a.size())
+
+	a := [...]int{1, 234, 89, 2314, 92, 784, 2}
+	fmt.Println(a)
+
+	num := len(a)
+
+	for i := 0; i < num; i++ {
+		for j := i + 1; j < num; j++ {
+			if a[i] > a[j] {
+				temp := a[i]
+				a[i] = a[j]
+				a[j] = temp
+			}
+		}
+	}
+	fmt.Println(a)
+}
